778b1a4d1e5422d3c38164fb26808cfa5743f455
ArjaniAleena <118487445+ArjaniAleena@users.noreply.github.com>
2022-12-27 17:13:18 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var student1
+var student2
+
+function setup() {
+  createCanvas(400,400);
+student1 = new Student("Aleena",12,7)
+student1.display()
+
+student2  = new Student ("Ava", 12,7)
+student2.display()
+
+}
+
+function draw() 
+{
+  background(30);
+  
+}
